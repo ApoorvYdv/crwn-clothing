3cace8b2b86379ed398b5db38512789a58ab40bf
Apoorv-17 <62233992+Apoorv-17@users.noreply.github.com>
2021-01-25 00:20:35 +0530

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -1,44 +1,41 @@
 import React from 'react';
-
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
       currentUser: null
-    }
+    };
   }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if(userAuth) {
+      if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
           this.setState({
-            currentUser: {  
+            currentUser: {
               id: snapShot.id,
               ...snapShot.data()
             }
           });
         });
-        console.log(this.state);
       }
-      else {
-        this.setState({ currentUser: userAuth});
-      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
@@ -49,11 +46,11 @@ class App extends React.Component {
   render() {
     return (
       <div>
-      <Header currentUser={this.state.currentUser} />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage}/>
+          <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
       </div>
     );
